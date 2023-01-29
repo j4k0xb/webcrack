78a7ccfee4a272fe80190a32ba4810d5bc64d3fa
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2023-01-29 05:19:10 +0100

    test: simpler string array expects

--- test/deobfuscator.test.ts
+++ test/deobfuscator.test.ts
@@ -6,39 +6,11 @@ import findStringArray from '../src/deobfuscator/stringArray';
 describe('find string array', async () => {
   const ast = parse(await readFile('./test/samples/obfuscator.io.js', 'utf8'));
 
-  test('type 1', () => {
+  test('function wrapper', () => {
     const stringArray = findStringArray(ast);
     expect(stringArray).toBeDefined();
+    expect(stringArray!.name).toBe('c');
     expect(stringArray!.references).toHaveLength(2);
-    expect(stringArray).toMatchObject({
-      name: 'c',
-      strings: [
-        'while (true) {}',
-        'apply',
-        'counter',
-        'debu',
-        'gger',
-        'call',
-        '590708XPLBWj',
-        '931306ycAlTF',
-        '1212dZxdja',
-        '88goZCZH',
-        '10gkeCxS',
-        '1006854ASxxtT',
-        '4464481uvpzKS',
-        '8Jgsyfr',
-        '3685185hiRGbv',
-        '10xlLBXx',
-        '31674555rUfUve',
-        'function *\\( *\\)',
-        '\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)',
-        'init',
-        'test',
-        'chain',
-        'log',
-        'Hello World!',
-        'constructor',
-      ],
-    });
+    expect(stringArray!.strings).toHaveLength(25);
   });
 });
