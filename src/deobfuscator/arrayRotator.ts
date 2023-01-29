71246c9dad2d9060c27c773549720490f6febe0c
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2023-01-29 22:19:26 +0100

    refactor: move rotator

---
+++ src/deobfuscator/arrayRotator.ts
@@ -0,0 +1,78 @@
+import traverse, { NodePath } from '@babel/traverse';
+import * as t from '@babel/types';
+import * as m from '@codemod/matchers';
+
+interface ArrayRotator {
+  path: NodePath<t.ExpressionStatement>;
+}
+/**
+ * Structure:
+ * ```
+ * iife (>= 2 parameters, called with 0 or 2 arguments)
+ *  2 variable declarations (array and decoder)
+ *  endless loop:
+ *   try:
+ *    if/break/parseInt/array.push(array.shift())
+ *   catch:
+ *    array.push(array.shift())
+ * ```
+ */
+export function findArrayRotator(ast: t.Node) {
+  let result: ArrayRotator | undefined;
+
+  traverse(ast, {
+    ExpressionStatement(path) {
+      if (matcher.match(path.node)) {
+        result = { path };
+        path.stop();
+      }
+    },
+  });
+
+  return result;
+}
+
+// e.g. 'array'
+const arrayName = m.capture(m.anyString());
+// e.g. array.push(array.shift())
+const pushShift = m.callExpression(
+  m.memberExpression(
+    m.identifier(m.fromCapture(arrayName)),
+    m.identifier('push')
+  ),
+  [
+    m.callExpression(
+      m.memberExpression(
+        m.identifier(m.fromCapture(arrayName)),
+        m.identifier('shift')
+      )
+    ),
+  ]
+);
+
+const matcher = m.expressionStatement(
+  m.callExpression(
+    m.functionExpression(
+      null,
+      m.anything(),
+      m.blockStatement(
+        m.anyList<t.Statement>(
+          // = decodeFn
+          m.variableDeclaration(),
+          // = array
+          m.variableDeclaration(undefined, [
+            m.variableDeclarator(m.identifier(arrayName)),
+          ]),
+          m.zeroOrMore(),
+          // TODO: preprocess and check the more general while(true) case
+          m.forStatement(
+            null,
+            null,
+            null,
+            m.tryStatement(m.containerOf(pushShift), m.containerOf(pushShift))
+          )
+        )
+      )
+    )
+  )
+);
