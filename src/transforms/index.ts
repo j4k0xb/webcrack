836c3483210e22f6c07d0b92781e55396e26da39
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2023-01-30 03:08:04 +0100

    feat: simplify string literals

--- src/transforms/index.ts
+++ src/transforms/index.ts
@@ -1,10 +1,12 @@
 import { TraverseOptions } from '@babel/traverse';
 import blockStatement from './blockStatement';
 import computedProperties from './computedProperties';
+import literals from './literals';
 import sequence from './sequence';
 import splitVariableDeclarations from './splitVariableDeclarations';
 
 export const transforms: Transform[] = [
+  literals,
   blockStatement,
   computedProperties,
   sequence,
---
+++ src/transforms/literals.ts
@@ -0,0 +1,17 @@
+import { Transform } from '.';
+
+export default {
+  name: 'literals',
+  tags: ['safe'],
+  visitor: {
+    StringLiteral(path) {
+      delete path.node.extra;
+      this.changes++;
+    },
+    NumericLiteral(path) {
+      delete path.node.extra;
+      this.changes++;
+    },
+    noScope: true,
+  },
+} satisfies Transform;
