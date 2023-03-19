4509a6695dfa46170e329ca657ad4a07001c08e6
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2023-03-19 02:33:19 +0100

    test: add control flow sample

---
+++ test/samples/obfuscator.io-control-flow.js
@@ -0,0 +1,56 @@
+function b(c, d) {
+  const e = a();
+  b = function (f, g) {
+      f = f - 0x0;
+      let h = e[f];
+      return h;
+  };
+  return b(c, d);
+}
+function a() {
+  const h = [
+      '1475288sftbSy',
+      'log',
+      '4xrHVEW',
+      '6374744oDMzgx',
+      'Hello\x20World!',
+      '4400046uAAZcD',
+      '1424131sNeoWn',
+      'vUosf',
+      '4860894fjGMlb',
+      '175343wKcRPS',
+      'JhtFX',
+      '1949560NCeCZi'
+  ];
+  a = function () {
+      return h;
+  };
+  return a();
+}
+(function (c, d) {
+  const e = c();
+  while (!![]) {
+      try {
+          const f = parseInt(b(0x4)) / 0x1 + parseInt(b(0xa)) / 0x2 + parseInt(b(0x6)) / 0x3 * (-parseInt(b(0x0)) / 0x4) + parseInt(b(0x9)) / 0x5 + parseInt(b(0x3)) / 0x6 + -parseInt(b(0x7)) / 0x7 + -parseInt(b(0x1)) / 0x8;
+          if (f === d) {
+              break;
+          } else {
+              e['push'](e['shift']());
+          }
+      } catch (g) {
+          e['push'](e['shift']());
+      }
+  }
+}(a, 0xcdc56));
+function hi() {
+  const c = {
+      'vUosf': function (f, g) {
+          return f < g;
+      },
+      'JhtFX': b(0x2)
+  };
+  const d = 0x1, e = 0x2;
+  if (c[b(0x5)](d, e))
+      console[b(0xb)](c[b(0x8)]);
+}
+hi();
