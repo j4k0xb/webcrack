62effef2c3c8e991b87430d18bf0c1774759ef60
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2023-03-29 21:42:39 +0200

    feat: remove leftover empty iife

--- src/deobfuscator/selfDefending.ts
+++ src/deobfuscator/selfDefending.ts
@@ -8,13 +8,13 @@ import {
   trueMatcher,
 } from '../utils/matcher';
 
-// SingleCallController: // https://github.com/javascript-obfuscator/javascript-obfuscator/blob/d7f73935557b2cd15a2f7cd0b01017d9cddbd015/src/custom-code-helpers/common/templates/SingleCallControllerTemplate.ts
+// SingleCallController: https://github.com/javascript-obfuscator/javascript-obfuscator/blob/d7f73935557b2cd15a2f7cd0b01017d9cddbd015/src/custom-code-helpers/common/templates/SingleCallControllerTemplate.ts
 
 // Works for
 // self defending: https://github.com/javascript-obfuscator/javascript-obfuscator/blob/d7f73935557b2cd15a2f7cd0b01017d9cddbd015/src/custom-code-helpers/self-defending/templates/SelfDefendingTemplate.ts
 // domain lock: https://github.com/javascript-obfuscator/javascript-obfuscator/blob/d7f73935557b2cd15a2f7cd0b01017d9cddbd015/src/custom-code-helpers/domain-lock/templates/DomainLockTemplate.ts
-//
 // console output: https://github.com/javascript-obfuscator/javascript-obfuscator/blob/d7f73935557b2cd15a2f7cd0b01017d9cddbd015/src/custom-code-helpers/console-output/templates/ConsoleOutputDisableTemplate.ts
+// debug protection function call: https://github.com/javascript-obfuscator/javascript-obfuscator/blob/d7f73935557b2cd15a2f7cd0b01017d9cddbd015/src/custom-code-helpers/debug-protection/templates/debug-protection-function-call/DebugProtectionFunctionCallTemplate.ts
 
 export default {
   name: 'selfDefending',
@@ -39,8 +39,10 @@ export default {
             removeSelfDefendingRefs(ref);
           }
 
+          // leftover (function () {})() from debug protection function call
+          ref.findParent(p => emptyIife.match(p.node))?.remove();
+
           this.changes++;
-          // TODO: possibly remove empty iife around the self-defending code
         });
 
       path.remove();
@@ -64,6 +66,10 @@ function removeSelfDefendingRefs(path: NodePath) {
     varDecl.remove();
   }
 }
+
+const emptyIife = m.callExpression(
+  m.functionExpression(null, [], m.blockStatement([]))
+);
 
 const callController = m.capture(m.anyString());
 const firstCall = m.capture(m.identifier());
