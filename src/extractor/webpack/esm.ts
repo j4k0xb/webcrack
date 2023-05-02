8884f7306d6a901acae5e120bdeff71cdb2c8831
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2023-05-02 22:53:29 +0200

    feat: webpack 5 esm

--- src/extractor/webpack/esm.ts
+++ src/extractor/webpack/esm.ts
@@ -1,4 +1,4 @@
-import traverse, { NodePath } from '@babel/traverse';
+import traverse, { NodePath, Scope } from '@babel/traverse';
 import * as t from '@babel/types';
 import * as m from '@codemod/matchers';
 import { constMemberExpression } from '../../utils/matcher';
@@ -41,6 +41,22 @@ export function convertESM(module: Module) {
     ])
   );
 
+  const properties = m.capture(
+    m.arrayOf(
+      m.objectProperty(
+        m.identifier(),
+        m.arrowFunctionExpression([], m.identifier())
+      )
+    )
+  );
+  // E.g. require.d(exports, { foo: () => a, bar: () => b });
+  const defineExportsMatcher = m.expressionStatement(
+    m.callExpression(constMemberExpression(m.identifier('require'), 'd'), [
+      m.identifier('exports'),
+      m.objectExpression(properties),
+    ])
+  );
+
   traverse(module.ast, {
     enter(path) {
       // Only traverse the top-level
@@ -52,34 +68,48 @@ export function convertESM(module: Module) {
         return;
       }
 
-      if (defineExportMatcher.match(path.node)) {
-        const binding = path.scope.getBinding(returnedName.current!);
-        if (!binding) return;
-
-        const declaration = binding.path.find(path =>
-          path.isDeclaration()
-        ) as NodePath<
-          t.VariableDeclaration | t.ClassDeclaration | t.FunctionDeclaration
-        > | null;
-        if (!declaration) return;
-
-        if (exportedName.current === 'default') {
-          // `let f = 1;` -> `export default 1;`
-          declaration.replaceWith(
-            t.exportDefaultDeclaration(
-              t.isVariableDeclaration(declaration.node)
-                ? declaration.node.declarations[0].init!
-                : declaration.node
-            )
-          );
-        } else {
-          // `let f = 1;` -> `export let counter = 1;`
-          renameFast(binding, exportedName.current!);
-          declaration.replaceWith(t.exportNamedDeclaration(declaration.node));
+      if (defineExportsMatcher.match(path.node)) {
+        for (const property of properties.current!) {
+          const exportName = (property.key as t.Identifier).name;
+          const returnedName = ((property.value as t.ArrowFunctionExpression).body as t.Identifier).name;
+          exportVariable(path.scope, returnedName, exportName);
         }
-
+        path.remove();
+      } else if (defineExportMatcher.match(path.node)) {
+        exportVariable(
+          path.scope,
+          returnedName.current!,
+          exportedName.current!
+        );
         path.remove();
       }
     },
   });
+}
+
+function exportVariable(scope: Scope, varName: string, exportName: string) {
+  const binding = scope.getBinding(varName);
+  if (!binding) return;
+
+  const declaration = binding.path.find(path =>
+    path.isDeclaration()
+  ) as NodePath<
+    t.VariableDeclaration | t.ClassDeclaration | t.FunctionDeclaration
+  > | null;
+  if (!declaration) return;
+
+  if (exportName === 'default') {
+    // `let f = 1;` -> `export default 1;`
+    declaration.replaceWith(
+      t.exportDefaultDeclaration(
+        t.isVariableDeclaration(declaration.node)
+          ? declaration.node.declarations[0].init!
+          : declaration.node
+      )
+    );
+  } else {
+    // `let f = 1;` -> `export let counter = 1;`
+    renameFast(binding, exportName);
+    declaration.replaceWith(t.exportNamedDeclaration(declaration.node));
+  }
 }
