014944a37131f98a09f91253231b3daa91e96a05
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2023-05-18 01:53:56 +0200

    feat: reconstruct dependency file structure

--- src/extractor/browserify/index.ts
+++ src/extractor/browserify/index.ts
@@ -1,20 +1,22 @@
 import traverse, { NodePath } from '@babel/traverse';
 import * as t from '@babel/types';
 import * as m from '@codemod/matchers';
+import { getPropName } from '../../utils/ast';
 import { constKey } from '../../utils/matcher';
+import { resolveDependencyTree } from '../../utils/path';
 import { renameParameters } from '../../utils/rename';
-import { Bundle } from '../bundle';
-import { Module } from '../module';
+import { BrowserifyBundle } from './bundle';
+import { BrowserifyModule } from './module';
 
-export function extract(ast: t.Node): Bundle | undefined {
-  const modules = new Map<number, Module>();
+export function extract(ast: t.Node): BrowserifyBundle | undefined {
+  const modules = new Map<number, BrowserifyModule>();
 
   const files = m.capture(
     m.arrayOf(
       m.objectProperty(
         m.numericLiteral(),
         m.arrayExpression([
-          // module function wrapper
+          // function(require, module, exports) {...}
           m.functionExpression(),
           // dependencies: { './add': 1, 'lib': 3 }
           m.objectExpression(
@@ -24,17 +26,22 @@ export function extract(ast: t.Node): Bundle | undefined {
       )
     )
   );
-  const cache = m.objectExpression();
   const entryId = m.capture(m.numericLiteral());
 
+  // (function (files, cache, entryIds) {...})(...)
   const matcher = m.callExpression(
     m.functionExpression(undefined, [
       m.identifier(),
       m.identifier(),
       m.identifier(),
     ]),
-    [m.objectExpression(files), cache, m.arrayExpression([entryId])]
+    [
+      m.objectExpression(files),
+      m.objectExpression(),
+      m.arrayExpression([entryId]),
+    ]
   );
+
   traverse(ast, {
     CallExpression(path) {
       if (!matcher.match(path.node)) return;
@@ -43,28 +50,49 @@ export function extract(ast: t.Node): Bundle | undefined {
         files.currentKeys!.join('.')
       ) as NodePath<t.ObjectProperty>[];
 
+      const dependencyTree: Record<number, Record<number, string>> = {};
+
       for (const moduleWrapper of modulesPath) {
         const id = (moduleWrapper.node.key as t.NumericLiteral).value;
         const fn = moduleWrapper.get(
           'value.elements.0'
         ) as NodePath<t.FunctionExpression>;
 
-        // TODO: handle dependencies
-        const dependencies = moduleWrapper.get(
-          'value.elements.1'
-        ) as NodePath<t.ObjectExpression>;
+        const dependencies: Record<number, string> = (dependencyTree[id] = {});
+        const dependencyProperties = (
+          moduleWrapper.get('value.elements.1') as NodePath<t.ObjectExpression>
+        ).node.properties as t.ObjectProperty[];
+
+        for (const dependency of dependencyProperties) {
+          const filePath = getPropName(dependency.key)!;
+          const id = (dependency.value as t.NumericLiteral).value;
+          dependencies[id] = filePath;
+        }
 
         renameParameters(fn, ['require', 'module', 'exports']);
         const file = t.file(t.program(fn.node.body.body));
-        const module = new Module(id, file, id === entryId.current!.value);
+        const module = new BrowserifyModule(
+          id,
+          file,
+          id === entryId.current!.value,
+          dependencies
+        );
         modules.set(id, module);
+      }
+
+      const resolvedPaths = resolveDependencyTree(
+        dependencyTree,
+        entryId.current!.value
+      );
+
+      for (const module of modules.values()) {
+        module.path = resolvedPaths[module.id];
       }
     },
     noScope: true,
   });
 
   if (modules.size > 0) {
-    return new Bundle('browserify', entryId.current!.value, modules);
+    return new BrowserifyBundle(entryId.current!.value, modules);
   }
-  return;
 }
--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,6 +1,68 @@
-import { dirname, relative } from 'node:path';
+import assert from 'node:assert';
+import { dirname, join, relative } from 'node:path';
 
 export function relativePath(from: string, to: string) {
   const relativePath = relative(dirname(from), to);
   return relativePath.startsWith('.') ? relativePath : './' + relativePath;
+}
+
+/**
+ * Resolve the path of each module of a browserify bundle
+ * based on its dependencies.
+ * @param tree module id -> dependencies (id -> path)
+ * @param entry entry module id
+ */
+export function resolveDependencyTree(
+  tree: Record<number, Record<number, string>>,
+  entry: number
+): Record<number, string> {
+  const paths = resolveTreePaths(tree, entry);
+  paths[entry] = './index.js';
+
+  const entryDepth = Object.values(paths).reduce(
+    (acc, path) => Math.max(acc, path.split('..').length),
+    0
+  );
+  // If the entrypoint is in a subfolder, we need to make up a prefix to get rid of the `../`
+  const prefix = Array(entryDepth - 1)
+    .fill(0)
+    .map((_, i) => `tmp${i}`)
+    .join('/');
+
+  return Object.fromEntries(
+    Object.entries(paths).map(([id, path]) => {
+      const newPath = path.startsWith('node_modules/')
+        ? path
+        : join(prefix, path);
+      assert(!newPath.includes('..'));
+      return [id, newPath];
+    })
+  );
+}
+
+/**
+ * Recursively resolve the paths of a dependency tree.
+ */
+function resolveTreePaths(
+  graph: Record<number, Record<number, string>>,
+  entry: number,
+  cwd = '.'
+) {
+  const paths: Record<number, string> = {};
+  const entries = Object.entries(graph[entry]) as unknown as [number, string][];
+
+  for (const [id, key] of entries) {
+    let path: string;
+    if (key.startsWith('.')) {
+      path = join(cwd, key);
+      if (!path.endsWith('.js')) path += '.js';
+    } else {
+      path = join('node_modules', key, 'index.js');
+    }
+    paths[id] = path;
+    const newCwd = path.endsWith('.js') ? dirname(path) : path;
+    Object.assign(paths, resolveTreePaths(graph, id, newCwd));
+  }
+
+  return paths;
 }
