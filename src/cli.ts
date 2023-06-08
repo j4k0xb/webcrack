4d0997e838fd351d680a31916b89529f1725674e
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2023-06-08 04:38:01 +0200

    feat: allow stdin/stdout for cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,41 +1,61 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import debug from 'debug';
 import { existsSync, readFileSync } from 'node:fs';
 import { readFile, rm } from 'node:fs/promises';
 import { join } from 'node:path';
-import * as url from 'url';
+import * as url from 'node:url';
 import { webcrack } from './index.js';
-import debug from 'debug';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-
 const { version, description } = JSON.parse(
   readFileSync(join(__dirname, '..', 'package.json'), 'utf8')
 ) as { version: string; description: string };
 
+debug.enable('webcrack:*');
+
 interface Options {
   force: boolean;
-  output: string;
+  output?: string;
+}
+
+async function readStdin() {
+  let data = '';
+  process.stdin.setEncoding('utf8');
+  for await (const chunk of process.stdin) data += chunk;
+  return data;
 }
 
 program
   .version(version)
   .description(description)
-  .option('-o, --output <path>', 'output directory', 'webcrack-out')
+  .option('-o, --output <path>', 'output directory for bundled files')
   .option('-f, --force', 'overwrite output directory')
-  .argument('<file>', 'input file')
-  .action(async (input: string) => {
+  .argument('[file]', 'input file, defaults to stdin')
+  .action(async (input: string | undefined) => {
     const { output, force } = program.opts<Options>();
-    const code = await readFile(input, 'utf8');
+    const code = await (input ? readFile(input, 'utf8') : readStdin());
 
-    if (force || !existsSync(output)) {
-      await rm(output, { recursive: true, force: true });
-    } else {
-      program.error('output directory already exists');
+    if (output) {
+      if (force || !existsSync(output)) {
+        await rm(output, { recursive: true, force: true });
+      } else {
+        program.error('output directory already exists');
+      }
     }
-    debug.enable('webcrack:*');
+
     const result = await webcrack(code);
-    await result.save(output);
+
+    if (output) {
+      await result.save(output);
+    } else {
+      console.log(result.code);
+      if (result.bundle) {
+        debug('webcrack:unpack')(
+          `${result.bundle.modules.size} modules are not displayed in the terminal. Use the --output option to save them`
+        );
+      }
+    }
   })
   .parse();
