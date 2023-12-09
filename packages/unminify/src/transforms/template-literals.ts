4969d3a454a751d5d30526ed82ae8845d443a789
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2023-12-09 22:40:47 +0100

    refactor(unminify): transform .concat to template literal

--- packages/unminify/src/transforms/template-literals.ts
+++ packages/unminify/src/transforms/template-literals.ts
@@ -1,83 +1,90 @@
 import * as t from '@babel/types';
 import * as m from '@codemod/matchers';
-import { Transform } from '@webcrack/ast-utils';
+import { Transform, constMemberExpression } from '@webcrack/ast-utils';
+
+// https://github.com/babel/babel/pull/5791
+// https://github.com/babel/babel/blob/cce807f1eb638ee3030112dc190cbee032760888/packages/babel-plugin-transform-template-literals/src/index.ts
+
+// TODO: option ignoreToPrimitiveHint (uses `+` instead of concat)
 
 function escape(str: string) {
-  return str.replaceAll('`', '\\`').replaceAll('${', '\\${');
+  return str
+    .replace(/\\/g, '\\\\')
+    .replace(/`/g, '\\`')
+    .replace(/\${/g, '\\${')
+    .replace(/\t/g, '\\t')
+    .replace(/\r/g, '\\r');
 }
 
-function hasEscapedChar(str: string) {
-  return str.includes('\\`') || str.includes('\\$');
+function flattenConcats(node: t.CallExpression) {
+  const parts: t.Expression[] = [];
+  let current: t.Expression = node;
+  while (current.type === 'CallExpression') {
+    parts.unshift(...(current.arguments as t.Expression[]));
+    current = (current.callee as t.MemberExpression).object;
+  }
+  parts.unshift(current);
+  return parts;
 }
 
 export default {
   name: 'template-literals',
-  tags: ['safe'],
+  tags: ['unsafe'],
   visitor() {
-    const noLiteral = m.matcher<t.Expression>((node) => !t.isLiteral(node));
-
-    const concatMatcher = m.or(
-      m.binaryExpression('+', noLiteral, m.stringLiteral()),
-      m.binaryExpression('+', m.stringLiteral(), noLiteral),
+    const concatMatcher: m.Matcher<t.CallExpression> = m.or(
+      m.callExpression(
+        constMemberExpression(
+          m.or(
+            m.stringLiteral(),
+            m.matcher((node) => concatMatcher.match(node)),
+          ),
+          'concat',
+        ),
+        m.arrayOf(m.anyExpression()),
+      ),
     );
-    const templateConcatMatcher = m.binaryExpression('+', m.templateLiteral());
 
     return {
       StringLiteral(path) {
         // Heuristic: source code most likely used a template literal if it contains multiple lines
         if (!/\n.*?\n/.test(path.node.value)) return;
-        if (hasEscapedChar(path.node.value)) return;
-        if (concatMatcher.match(path.parent)) return;
+        if (concatMatcher.match(path.parentPath.parent)) return;
 
         const raw = escape(path.node.value);
         const quasi = t.templateElement({ raw });
         path.replaceWith(t.templateLiteral([quasi], []));
         this.changes++;
       },
-      BinaryExpression: {
+      CallExpression: {
         exit(path) {
-          if (concatMatcher.match(path.node)) {
-            const emptyQuasi = t.templateElement({ raw: '' });
-            const template = t.templateLiteral([emptyQuasi], []);
+          if (
+            concatMatcher.match(path.node) &&
+            !concatMatcher.match(path.parentPath.parent)
+          ) {
+            const parts = flattenConcats(path.node);
+            const quasis: t.TemplateElement[] = [];
+            const expressions: t.Expression[] = [];
 
-            if (path.node.left.type === 'StringLiteral') {
-              if (hasEscapedChar(path.node.left.value)) return;
-              // 'a' + b -> `a${b}`
+            for (let i = 0; i < parts.length; i++) {
+              const part = parts[i];
+              const nextPart = parts[i + 1];
+              const followedByString = nextPart?.type === 'StringLiteral';
 
-              const raw = escape(path.node.left.value);
-              template.quasis.unshift(t.templateElement({ raw }));
-              template.expressions.push(path.node.right);
-            } else if (path.node.right.type === 'StringLiteral') {
-              if (hasEscapedChar(path.node.right.value)) return;
-              // a + 'b' -> `${a}b`
-
-              const raw = escape(path.node.right.value);
-              template.quasis.push(t.templateElement({ raw }));
-              template.expressions.push(path.node.left as t.Expression);
-            }
-            path.replaceWith(template);
-            this.changes++;
-          } else if (templateConcatMatcher.match(path.node)) {
-            const expression = (path.node as t.BinaryExpression).right;
-            const template = (path.node as t.BinaryExpression)
-              .left as t.TemplateLiteral;
-
-            if (expression.type === 'StringLiteral') {
-              if (hasEscapedChar(expression.value)) return;
-              // `a${b}` + 'c' -> `a${b}c`
-
-              const raw = escape(expression.value);
-              const quasi = t.templateElement({ raw });
-              if (template.quasis.at(-1)?.value.raw === '') {
-                template.quasis.pop();
+              if (part.type === 'StringLiteral') {
+                if (followedByString) {
+                  nextPart.value = part.value + nextPart.value;
+                } else {
+                  quasis.push(t.templateElement({ raw: escape(part.value) }));
+                }
+              } else {
+                expressions.push(part);
+                if (!followedByString) {
+                  quasis.push(t.templateElement({ raw: '' }));
+                }
               }
-              template.quasis.push(quasi);
-            } else {
-              // `a${b}` + c -> `a${b}${c}`
-              template.expressions.push(expression);
-              template.quasis.push(t.templateElement({ raw: '' }));
             }
 
+            const template = t.templateLiteral(quasis, expressions);
             path.replaceWith(template);
             this.changes++;
           }
--- packages/unminify/test/template-literals.test.ts
+++ packages/unminify/test/template-literals.test.ts
@@ -4,44 +4,20 @@ import { templateLiterals } from '../src/transforms';
 
 const expectJS = testTransform(templateLiterals);
 
-test('convert string with new lines', () =>
-  expectJS('const a = "a\\nb\\nc";').toMatchInlineSnapshot(`
-    const a = \`a
-    b
-    c\`;
+test('escape quotes', () =>
+  expectJS(
+    `"'".concat(foo, "' \\"").concat(bar, "\\"");`,
+  ).toMatchInlineSnapshot(`\`'\${foo}' "\${bar}"\`;`));
+
+test('expressions', () =>
+  expectJS(`
+    "".concat(1);
+    1 + "".concat(foo).concat(bar).concat(baz);
+    1 + "".concat(foo, "bar").concat(baz);
+    "".concat(1, f, "oo", true).concat(b, "ar", 0).concat(baz);
+  `).toMatchInlineSnapshot(`
+    \`\${1}\`;
+    1 + \`\${foo}\${bar}\${baz}\`;
+    1 + \`\${foo}bar\${baz}\`;
+    \`\${1}\${f}oo\${true}\${b}ar\${0}\${baz}\`;
   `));
-
-test('ignore string without new lines', () =>
-  expectJS('const a = "a";').toMatchInlineSnapshot('const a = "a";'));
-
-test('string concat with non-literal', () =>
-  expectJS('const a = "x" + y;').toMatchInlineSnapshot('const a = `x${y}`;'));
-
-test('non-literal concat with string', () =>
-  expectJS('const a = x + "y";').toMatchInlineSnapshot('const a = `${x}y`;'));
-
-test('nested concat', () =>
-  expectJS('const a = "x" + y + "z";').toMatchInlineSnapshot(
-    'const a = `x${y}z`;',
-  ));
-
-test('escape dollar sign', () =>
-  expectJS('const a = "a\\n${b}\\nc";').toMatchInlineSnapshot(`
-    const a = \`a
-    \\\${b}
-    c\`;
-  `));
-
-test('escape backtick', () =>
-  expectJS('const a = "a\\n`\\nc";').toMatchInlineSnapshot(`
-    const a = \`a
-    \\\`
-    c\`;
-  `));
-
-// TODO: Needs more complex logic
-test.skip('convert pre-escaped backtick', () =>
-  expectJS('const a = "a\\n\\\\`\\nc";').toMatchInlineSnapshot());
-
-test.skip('convert pre-escaped dollar sign', () =>
-  expectJS('const a = "a\\n\\\\${b}\\nc";').toMatchInlineSnapshot());
