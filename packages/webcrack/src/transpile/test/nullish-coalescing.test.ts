1c6d171ccf2e43d898173e7e75d4c88563bf5706
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2023-12-27 06:24:50 +0100

    test: add more failing nullish coalescing cases

--- packages/webcrack/src/transpile/test/nullish-coalescing.test.ts
+++ packages/webcrack/src/transpile/test/nullish-coalescing.test.ts
@@ -40,19 +40,31 @@ test('member expression loose (Babel)', () =>
     var foo = opts.foo ?? "default";
   `));
 
-test.skip('member expression (esbuild)', () =>
+test.todo('member expression (esbuild)', () =>
   expectJS(`
     var _a_b;
     ((_a_b = a.b) !== null && _a_b !== undefined) || c;
   `).toMatchInlineSnapshot(`
     a.b ?? c;
-  `));
+  `),
+);
 
-test.skip('default param (Babel)', () =>
+test.todo('default param (Babel)', () =>
   expectJS(`
     function foo(foo, qux = (_foo$bar => (_foo$bar = foo.bar) !== null && _foo$bar !== void 0 ? _foo$bar : "qux")()) {}
     function bar(bar, qux = bar !== null && bar !== void 0 ? bar : "qux") {}
   `).toMatchInlineSnapshot(`
     function foo(foo, qux = foo.bar ?? "qux") {}
     function bar(bar, qux = bar ?? "qux") {}
-  `));
+  `),
+);
+
+// TODO: add unminify transform or different matchers?
+test.todo('flipped', () =>
+  expectJS(`
+    var interp;
+    (interp = hello) == null ? "" : interp;
+  `).toMatchInlineSnapshot(`
+    hello ?? "";
+  `),
+);
