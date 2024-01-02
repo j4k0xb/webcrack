b8d32a4dee23a7a021b9910ca650499babe65faf
Kuizuo <hi@kuizuo.cn>
2024-01-02 23:37:24 +0800

    feat: split variable declarations in for loop (#37)

--- packages/webcrack/src/unminify/test/split-variable-declarations.test.ts
+++ packages/webcrack/src/unminify/test/split-variable-declarations.test.ts
@@ -22,11 +22,42 @@ test('split exported variable declarations', () =>
     export const c = 3;
   `));
 
-test('dont split in for loop', () =>
+test('split var in for loop initializer', () =>
   expectJS(`
-    for (let i = 0, j = 1; i < 10; i++, j++) var a, b;
+    for (var i = 0, j = 1;;) {}
   `).toMatchInlineSnapshot(`
-    for (let i = 0, j = 1; i < 10; i++, j++) {
+    var i = 0;
+    var j = 1;
+    for (;;) {}
+  `));
+
+test('ignore let in for loop initializer', () => {
+  expectJS(`
+    let i;
+    for (let i = 0, j = 1;;) {}
+  `).toMatchInlineSnapshot(`
+    let i;
+    for (let i = 0, j = 1;;) {}
+  `);
+
+  expectJS(`
+    for (let i = 0, j = 1;;) {
+      setTimeout(() => console.log(i));
+      if (++i > 3) break;
+    }
+  `).toMatchInlineSnapshot(`
+    for (let i = 0, j = 1;;) {
+      setTimeout(() => console.log(i));
+      if (++i > 3) break;
+    }
+  `);
+});
+
+test('ignore for loop with test or update', () =>
+  expectJS(`
+    for (var i = 0, j = 1; i < 10; i++, j++) var a, b;
+  `).toMatchInlineSnapshot(`
+    for (var i = 0, j = 1; i < 10; i++, j++) {
       var a;
       var b;
     }
