069cb494c725c293424536db1dd048c05c027eb0
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2024-01-13 03:20:27 +0100

    test: add nested var injections

--- packages/webcrack/src/unpack/test/var-injections.test.ts
+++ packages/webcrack/src/unpack/test/var-injections.test.ts
@@ -15,6 +15,20 @@ test('replace', () =>
     console.log(m, n);
   `));
 
+test('nested var injections', () =>
+  expectJS(`
+    (function (m, n) {
+      (function (o) {
+        console.log(m, n, o);
+      }.call(this, __webpack_require__(3)));
+    }.call(this, __webpack_require__(1), __webpack_require__(2)));
+  `).toMatchInlineSnapshot(`
+    var m = __webpack_require__(1);
+    var n = __webpack_require__(2);
+    var o = __webpack_require__(3);
+    console.log(m, n, o);
+  `));
+
 test('ignore different number of params and args', () =>
   expectJS(`
     (function (m, n) {
