f6df5f2c91fe5f848b9aa24dbcfa0b39b308a1e6
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2024-06-07 10:33:04 +0200

    fix: lifted tab css (#89)

--- apps/playground/src/components/Tab.tsx
+++ apps/playground/src/components/Tab.tsx
@@ -10,14 +10,14 @@ interface Props {
 export default function Tab(props: Props) {
   return (
     <a
-      class="tab flex-nowrap justify-between min-w-[120px] !pr-1 whitespace-nowrap"
+      class="tab flex-nowrap min-w-[120px] whitespace-nowrap"
       classList={{ 'tab-active': props.active, 'bg-base-200': !props.active }}
       title={props.path}
       onClick={props.onClick}
     >
       <span class="max-w-[200px] truncate">{basename(props.path)}</span>
       <button
-        class="btn btn-xs btn-ghost btn-circle ml-2"
+        class="btn btn-xs btn-ghost btn-circle ml-auto !translate-x-2"
         title="Close tab"
         onClick={(e) => {
           e.stopPropagation();
