2974a0c84f328447f6ca0bf7d33ecd0e7399aab9
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2024-06-08 13:51:44 +0200

    feat: open recent menu chevron (#91)

--- apps/playground/src/components/menu/MenuDropdown.tsx
+++ apps/playground/src/components/menu/MenuDropdown.tsx
@@ -11,6 +11,20 @@ export default function MenuDropdown(props: ParentProps<Props>) {
         <div tabindex="0" role="button">
           {props.title}
         </div>
+        <svg
+          class="ml-auto"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M9 6l6 6l-6 6" />
+        </svg>
         <ul
           tabindex="0"
           class="dropdown-content z-10 menu ml-0 p-2 shadow bg-base-100 rounded-box"
