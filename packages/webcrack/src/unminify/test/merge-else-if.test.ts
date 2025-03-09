1b7c4fe85bcdc2f1955f79a7b16ea42b6905d9a6
Leo Developer <leo@git.leodev.win>
2025-03-09 12:17:40 +0100

    merge branch in if

--- packages/webcrack/src/unminify/test/merge-else-if.test.ts
+++ packages/webcrack/src/unminify/test/merge-else-if.test.ts
@@ -4,12 +4,45 @@ import mergeElseIf from '../transforms/merge-else-if';
 
 const expectJS = testTransform(mergeElseIf);
 
-test('merge', () =>
+test('merge', () => {
   expectJS(`
     if (x) {
     } else {
       if (y) {}
-    }`).toMatchInlineSnapshot('if (x) {} else if (y) {}'));
+    }`).toMatchInlineSnapshot('if (x) {} else if (y) {}');
+
+  expectJS(`
+    if (!cond) {
+      if (cond2) {
+        console.log("branch 2");
+      } else {
+        console.log("branch 3");
+      }
+    } else {
+      console.log("branch 1");
+    }`).toMatchInlineSnapshot(`
+      if (!!cond) {
+        console.log("branch 1");
+      } else if (cond2) {
+        console.log("branch 2");
+      } else {
+        console.log("branch 3");
+      }
+    `);
+
+  expectJS(`
+    if (!cond) {
+      if (cond2) {
+        console.log("branch 2");
+      }
+    }`).toMatchInlineSnapshot(`
+      if (!cond) {
+        if (cond2) {
+          console.log("branch 2");
+        }
+      }
+    `);
+});
 
 test('ignore when it contains other statements', () =>
   expectJS(`
--- packages/webcrack/src/unminify/transforms/merge-else-if.ts
+++ packages/webcrack/src/unminify/transforms/merge-else-if.ts
@@ -1,4 +1,5 @@
 import * as m from '@codemod/matchers';
+import * as t from '@babel/types';
 import type { Transform } from '../../ast-utils';
 
 export default {
@@ -6,16 +7,32 @@ export default {
   tags: ['safe'],
   visitor() {
     const nestedIf = m.capture(m.ifStatement());
-    const matcher = m.ifStatement(
+    const matcherElse = m.ifStatement(
       m.anything(),
       m.anything(),
       m.blockStatement([nestedIf]),
+    );
+    const matchedIf = m.ifStatement(
+      m.anything(),
+      m.blockStatement([nestedIf]),
+      m.anything(),
     );
 
     return {
       IfStatement: {
         exit(path) {
-          if (matcher.match(path.node)) {
+          // if (cond) { ... } else { if (cond2) { ... } }
+          // -> if (cond) { ... } else if (!cond2) { ... }
+          if (matcherElse.match(path.node)) {
+            path.node.alternate = nestedIf.current;
+            this.changes++;
+          }
+
+          // if (cond) { if(cond2) { branch1 } else { branch2 } } else { branch3 }
+          // -> if (!cond) { branch3 } else if (cond2) { branch1 } else { branch2 }
+          if (matchedIf.match(path.node) && path.node.alternate) {
+            path.node.test = t.unaryExpression('!', path.node.test);
+            path.node.consequent = path.node.alternate!;
             path.node.alternate = nestedIf.current;
             this.changes++;
           }
