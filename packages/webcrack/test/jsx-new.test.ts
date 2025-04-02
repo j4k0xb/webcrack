2cd5bf5103bf6ded1afdfaa11f3334b2bef728f5
Alexey Elizarov <alex.elizarov1@gmail.com>
2025-04-02 19:00:29 +0300

    feat: deobfuscate object member jsx (#175)
    * feat: deobfuscate object member jsx
    
    * style: run `pnpm format`
    
    ---------
    
    Co-authored-by: j4k0xb <55899582+j4k0xb@users.noreply.github.com>

--- packages/webcrack/test/jsx-new.test.ts
+++ packages/webcrack/test/jsx-new.test.ts
@@ -93,3 +93,8 @@ test('props with escaped strings', () =>
 
 test('indirect jsx call', () =>
   expectJS('(0, r.jsx)("div", {})').toMatchInlineSnapshot('<div />;'));
+
+test('object member jsxs and jsx', () =>
+  expectJS(
+    'r.jsxs(Foo, { bar: 1, children: [r.jsx(Child, {})] });',
+  ).toMatchInlineSnapshot('<Foo bar={1}><Child /></Foo>;'));
